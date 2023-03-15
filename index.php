854e17c99e6cbb64e8b59e7753a7c60b7dd5dab9
Raghadsb <rbugshan0005@stu.kau.edu.sa>
2023-03-15 18:29:05 +0300

    m

--- index.php
+++ index.php
@@ -49,38 +49,7 @@
 			</div>
 		</div>
 	</section>
-	<?php
 
-require('vendor/autoload.php');
-$servername = "localhost";
-$username = "";
-$password = "";
-$dbname = "adminlog";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password,$dbname);
-
-// Check connection
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT * FROM `logincred` WHERE 1;";
-$result = $conn->query($sql);
-
-
-	$name="";
-	$idd="";
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-	$name=$row["Username"];
-	$idd=$row["Password"];
-    // echo "id: " . $row["ID"]. " - Name: " . $row["Username"]. " " . $row["Password"]. "<br>";
-  }
-} else {
-//   echo "0 results";
-}?>
     <script>
 function redirectgame(){
 	location.replace('http://localhost/login-form-18/login-form-18/theball.html');
@@ -118,4 +87,35 @@ else{
 
 	</body>
 </html>
+<?php
 
+require('vendor/autoload.php');
+$servername = "localhost";
+$username = "";
+$password = "";
+$dbname = "adminlog";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM `logincred` WHERE 1;";
+$result = $conn->query($sql);
+
+
+	$name="";
+	$idd="";
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+	$name=$row["Username"];
+	$idd=$row["Password"];
+    // echo "id: " . $row["ID"]. " - Name: " . $row["Username"]. " " . $row["Password"]. "<br>";
+  }
+} else {
+//   echo "0 results";
+}?>
